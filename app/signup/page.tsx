50111f6cad704f2833b135c949a7b6f1676a2197
GVAmaresh <reshama0302@gmail.com>
2024-08-01 13:43:56 +0530

    Updated warning message

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -12,11 +12,17 @@ export default function Page() {
         <SignUP />
       ) : (
         <>
-          <div className="bg-black h-screen pt-32 font-extrabold text-slate-500 text-center text-xl md:text-3xl">
-            This section is only for developers
-          </div>
-          <div className="bg-black h-screen pt-32 font-extrabold text-slate-500 text-center text-xl md:text-3xl">
-            This section is only for developers
+          <div className="bg-black h-screen flex justify-center pt-32">
+            <div className="font-medium text-slate-500 text-lg md:text-xl lg:text-2xl">
+              <ul className="list-disc pl-5">
+                <li className="pb-4">This section is only for developers</li>
+                <li className="pb-4">Comment out SignUP.tsx page for signup </li>
+                <li className="pb-4">
+                  Do not forget to comment out SignUP.tsx page after signup for
+                  safety
+                </li>
+              </ul>
+            </div>
           </div>
         </>
       )}
